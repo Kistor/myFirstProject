08b5708f6d7546a2ccfd4a7680604f142ff8e608
breskuni@gmail.com <breskuni@gmail.com>
2022-05-30 23:33:25 +0700

    Now, "Label" is random string from Quests.txt.

--- src/main/java/com/example/mymathtest/HelloController.java
+++ src/main/java/com/example/mymathtest/HelloController.java
@@ -3,12 +3,19 @@ package com.example.mymathtest;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.io.IOException;
+
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label welcomeText;
+    private final int questsSize = 6;
+
+
+    @FXML
+    protected void onHelloButtonClick() throws IOException {
+        FileWork fileWork = new FileWork();
+        int i = (int)(Math.random() *  questsSize + 1);
+        welcomeText.setText(fileWork.TakeNumStr(i));
     }
 }
