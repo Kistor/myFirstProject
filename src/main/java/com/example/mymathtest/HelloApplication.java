a85357dc67e9ae22c4f0fd3c9c034388cadca327
breskuni@gmail.com <breskuni@gmail.com>
2022-07-02 11:44:04 +0300

    add control around bombs by exceptions clear window.

--- src/main/java/com/example/mymathtest/HelloApplication.java
+++ src/main/java/com/example/mymathtest/HelloApplication.java
@@ -3,30 +3,40 @@ package com.example.mymathtest;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    public static Map map;
 
 
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 550, 440);
+       FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), map.size* map.size + 100, map.size* map.size + 100);
         stage.setTitle("Сапер v0.1");
+        Button[] cell = new Button[map.size* map.size];
+
+        System.out.println(map.size* map.size);
+
+
         stage.setScene(scene);
         stage.show();
+
     }
 
 
 
     public static void main(String[] args) {
+        map = new Map(10);
+        map.mapInit();
+        map.printMap();
 
         launch();
-        int[][] boxes = new int[10][10];
 
     }
 }
--- src/main/java/com/example/mymathtest/Map.java
+++ src/main/java/com/example/mymathtest/Map.java
@@ -1,26 +1,84 @@
 package com.example.mymathtest;
 
 public class Map {
+    int[][] map;
+    int size;
+    boolean[][] openMap;
 
-    public void mapInit(int[][] map) {
+    Map(){}
+
+    Map(int Size)
+    {
+        size = Size;
+        map = new int[size][size];
+        openMap = new boolean[size][size];
+    }
+
+
+    public void printMap()
+    {
+        for (int[] ints : map) {
+            for (int j = 0; j < map.length; j++) {
+                System.out.print(ints[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public void mapInit() {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map.length; j++) {
                 map[i][j] = 0;
+                openMap[i][j] = false;
             }
         }
 
         int coord;
         for (int i = 0; i < map.length; i++) {
             // рандом места для бомбы
-            coord = (int) (Math.random() * 100);
-            if (map[coord % 10][coord / 10] == 10) {
+            coord = (int) (Math.random() *size*size);
+            if (map[coord % size][coord / size] == 9) {
                 i--;
             } else {
-                int x = coord % 10;
-                int y = coord / 10;
-                map[x][y] = 10;
+                int x = coord % size;
+                int y = coord / size;
+                map[x][y] = 9;
 
-                // написать обозначения для
+                // написать контроллер для краев карты(исключения?)
+
+                try {
+                    if(map[x+1][y+1] != 9)
+                        map[x+1][y+1]++;
+                } catch (Exception ignored) {}
+                try {
+                    if(map[x][y+1] != 9)
+                        map[x][y+1]++;
+                } catch (Exception ignored) {}
+                try {
+                    if(map[x+1][y] != 9)
+                        map[x+1][y]++;
+                } catch (Exception ignored) {}
+                try {
+                    if(map[x-1][y-1] != 9)
+                        map[x-1][y-1]++;
+                }catch (Exception ignored){}
+                try {
+                    if(map[x][y-1] != 9)
+                        map[x][y-1]++;
+                }catch (Exception ignored){}
+                try {
+                    if(map[x-1][y] != 9)
+                        map[x-1][y]++;
+                }catch (Exception ignored){}
+                try {
+                    if(map[x-1][y+1] != 9)
+                        map[x-1][y+1]++;
+                }catch (Exception ignored){}
+                try {
+                    if(map[x+1][y-1] != 9)
+                        map[x+1][y-1]++;
+                }catch (Exception ignored){}
 
 
             }
